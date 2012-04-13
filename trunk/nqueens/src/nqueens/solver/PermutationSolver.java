7ca033c4788f68234c594676890c3d1655a62a99
hussain.mutawa@gmail.com <hussain.mutawa@gmail.com@85cfdfdc-4dde-79c5-ff6c-485e8d6b0813>
2012-04-13 15:17:21 +0000

    added permutation solver

---
+++ trunk/nqueens/src/nqueens/solver/PermutationSolver.java
@@ -0,0 +1,35 @@
+package nqueens.solver;
+
+import nqueens.Board;
+
+public class PermutationSolver extends AbstractSolver {
+
+	@Override
+	protected void solveBoard() {
+		final Board board = getBoard();
+    	int N = board.getBoardSize();
+    	int[]q = solution(N);
+    	for(int i=0;i<N;i++){
+    		board.getCell(i, q[i]).flipOccupied();
+    	}
+	}
+	static boolean safe(int[] q) {
+		int n = q.length;
+		for (int i = 0; i < n; i++) {
+			for (int j = i+1; j < n; j++) {
+				if (Math.abs(q[j] - q[i]) == j-i) return false; 
+			}
+		}
+		return true;
+	}
+	static int[]solution(int q){
+		int[]a=new int[q];
+		for(int i=0;i<a.length;a[i]=i++){}
+		for(final Permute permute=new Permute(a);permute.hasNext();){
+			a=permute.next();
+			if(safe(a)) break;
+		}
+		return a;
+	}
+
+}
